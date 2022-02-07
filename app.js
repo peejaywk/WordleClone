1f2c7f742ae1060858e5530be14b1a7178514c52
peejaywk <peejaywk@gmail.com>
2022-02-07 16:03:58 +0000

    Add keyboard and styles.

---
+++ app.js
@@ -0,0 +1,47 @@
+const tileDisplay = document.querySelector('.tile-container')
+const keyboard = document.querySelector('.key-container')
+
+const keys = [
+    'Q',
+    'W',
+    'E',
+    'R',
+    'T',
+    'Y',
+    'U',
+    'I',
+    'O',
+    'P',
+    'A',
+    'S',
+    'D',
+    'F',
+    'G',
+    'H',
+    'J',
+    'K',
+    'L',
+    'ENTER',
+    'Z',
+    'X',
+    'C',
+    'V',
+    'B',
+    'N',
+    'M',
+    '<',
+]
+
+const handleClick = () => {
+    console.log('clicked')
+}
+
+// Create buttons using the keys array and assign attributes and event listeners.
+// Add buttons to the keyboard container.
+keys.forEach(key => {
+    const buttonElement = document.createElement('button')
+    buttonElement.textContent = key
+    buttonElement.setAttribute('id', key)
+    buttonElement.addEventListener('click', handleClick)
+    keyboard.append(buttonElement)
+})
